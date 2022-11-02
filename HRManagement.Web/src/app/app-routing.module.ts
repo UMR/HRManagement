97d92e105c134252224c3619504e12a73db95725
Towsif Patwary Shuvo <tpatwary@ael-bd.com>
2022-11-02 20:50:09 +0600

    Design login, register, home pages

--- HRManagement.Web/src/app/app-routing.module.ts
+++ HRManagement.Web/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'page-not-found',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: "/page-not-found"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- HRManagement.Web/src/app/app.module.ts
+++ HRManagement.Web/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+import { NoticeComponent } from './notice/notice.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    NoticeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
