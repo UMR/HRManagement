bf952663b78474024236852e400e5f9d5597faff
Towsif Patwary Shuvo <tpatwary@ael-bd.com>
2022-11-07 21:00:33 +0600

    registration page validations

--- HRManagement.Web/src/app/app.module.ts
+++ HRManagement.Web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,9 @@ import { NoticeService } from './notice/notice.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [NoticeService],
   bootstrap: [AppComponent]
--- HRManagement.Web/src/app/registration/registration.component.ts
+++ HRManagement.Web/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +9,57 @@ import { Router } from '@angular/router';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  registerForm: any;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstname: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      lastname: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      emailId: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl(null, [Validators.required])
+    },
+    {
+      validators: this.Mismatch('password', 'confirmPassword')
+    });
+  }
+
+  get firstname() { return this.registerForm.get('firstname'); }
+  get lastname() { return this.registerForm.get('lastname'); }
+  get emailId() { return this.registerForm.get('emailId'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+
+  Mismatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['Mismatch']) {
+        return
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ Mismatch : true});
+      }
+      else {
+        matchingControl.setErrors(null);
+      }
+    }
   }
 
   goToPage(pageName: any): void {
     this.router.navigate([`${pageName}`]);
   }
 
+  submitForm() {
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      alert(`${this.registerForm.value.firstname} has been registered.`);
+      this.registerForm.reset();
+    }
+  }
 }
