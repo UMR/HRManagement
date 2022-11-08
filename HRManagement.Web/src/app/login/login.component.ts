a226e255e6ba0f168ba8bcfff888accad0513119
Towsif Patwary Shuvo <tpatwary@ael-bd.com>
2022-11-08 17:47:43 +0600

    login page validations

--- HRManagement.Web/src/app/login/login.component.ts
+++ HRManagement.Web/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Login } from './login';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userModal = new Login();
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    alert(
+      `${this.userModal.emailId} Logged in Successfully!!`
+    );
+    console.log(this.userModal);
   }
 
   goToPage(pageName: any): void {
