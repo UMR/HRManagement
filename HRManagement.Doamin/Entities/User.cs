70ac8e7fc1f3a795a4cab55c96a52136bbf7891d
Md. Atikur Zaman <arzaman@ael-bd.com>
2022-11-17 12:17:12 +0600

    add global using

--- HRManagement.Doamin/Entities/User.cs
+++ HRManagement.Doamin/Entities/User.cs
@@ -1,13 +1,10 @@
-﻿using HRManagement.Domain.Common;
+﻿namespace HRManagement.Domain.Entities;
 
-namespace HRManagement.Domain.Entities
-{
-    public class User: BaseAuditableEntity
-    {        
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-       
-    }
+public class User: BaseAuditableEntity
+{        
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+   
 }
---
+++ HRManagement.Doamin/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using HRManagement.Domain.Common;
+global using HRManagement.Domain.Entities;
+global using HRManagement.Domain.Exceptions;
+
+
+
