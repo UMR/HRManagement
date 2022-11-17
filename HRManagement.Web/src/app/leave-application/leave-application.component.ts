c328ff53782c8add3e0bef50a3b00f1b6f0fd163
Towsif Patwary Shuvo <tpatwary@ael-bd.com>
2022-11-17 20:28:03 +0600

    Leave application v2

--- HRManagement.Web/src/app/leave-application/leave-application.component.ts
+++ HRManagement.Web/src/app/leave-application/leave-application.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+interface leavetype {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-leave-application',
@@ -6,10 +12,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leave-application.component.css']
 })
 export class LeaveApplicationComponent implements OnInit {
+  public leaveForm: any;
 
-  constructor() { }
+  public _leavetype: leavetype[] = [];
+  leavetype!: string;
+  
+  constructor(private formBuilder: FormBuilder)
+  {
+    this._leavetype = [
+      { code: '1', name: 'Sick Leave' },
+      { code: '2', name: 'Special Leave' },
+      { code: '3', name: 'Casual Leave' }
+    ];
+  }
 
   ngOnInit(): void {
+    this.leaveForm = this.formBuilder.group({
+      leaveFrom: new FormControl(null, [Validators.required]),
+      leaveTo: new FormControl(null, [Validators.required]), 
+      noOfDays: new FormControl(null, [Validators.required, Validators.pattern("^[0-9]*$")]),
+      leavetype: new FormControl(null, [Validators.required]),
+      contactnumber: new FormControl(null, [Validators.required, Validators.pattern("^[0-9]*$")]),
+      reasonforLeave: new FormControl(null),
+      approver: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+    });
+  }
+  get leaveFrom() { return this.leaveForm.get('leaveFrom'); }
+  get leaveTo() { return this.leaveForm.get('leaveTo'); }
+  get noOfDays() { return this.leaveForm.get('noOfDays'); }
+  get contactnumber() { return this.leaveForm.get('contactnumber'); } 
+  get reasonforLeave() { return this.leaveForm.get('reasonforLeave'); } 
+  get approver() { return this.leaveForm.get('reasonforLeave'); }
+
+  submitForm() {
+    console.log(this.leaveForm.value);
+
+    if (this.leaveForm.valid) {
+      this.leaveForm.reset();
+    }
   }
 
 }
